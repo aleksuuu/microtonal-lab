c922797ab828ef813cfd8d52c4824a5efaee6017
aleksuuu <qwu2@berklee.edu>
2024-12-23 18:00:04 +0800

    add base url to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Interval from "./pages/Interval";
 import NavBar from "./components/NavBar";
